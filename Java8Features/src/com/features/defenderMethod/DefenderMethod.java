114682ef19438f3205e984b32d0170b593076a9d
rrayappa <rrayappa@RRAYAPPA9GWFQ12.opentext.net>
2015-07-02 09:55:28 +0530

    Updated Desc

--- Java8Features/src/com/features/defenderMethod/DefenderMethod.java
+++ Java8Features/src/com/features/defenderMethod/DefenderMethod.java
@@ -1,5 +1,10 @@
 package com.features.defenderMethod;
 
+
+/*
+ * We can provide default implementation for the method by using default 
+ * Implemented class no need to implement this interface
+ * */
 public interface DefenderMethod {
 
 	public void show();
