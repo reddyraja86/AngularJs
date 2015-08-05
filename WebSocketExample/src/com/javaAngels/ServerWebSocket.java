7027174f68af24f54464b9cb0fdbd2d9ce92d275
reddyraja86 <reddyraja86@gmail.com>
2015-08-05 16:54:58 +0530

    Delete WebSockets
    Change-Id: I319f2a755763c5b078e79a720f6fc12a5209ed98
    Signed-off-by: rrayappa 
    Signed-off-by: rrayappa

---
+++ WebSocketExample/src/com/javaAngels/ServerWebSocket.java
@@ -0,0 +1,38 @@
+package com.javaAngels;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+
+
+@ServerEndpoint(value = "/ServerWebSocket")
+public class ServerWebSocket {
+
+    
+ 
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println("-- THIS IS onOpen or Onconnect method  ---"+session.hashCode());
+        System.out.println("=== HEADER INFORMATION======");
+        
+        
+        
+    }
+    
+    @OnMessage
+    public void onMessage(String message,Session session) throws Exception{
+        System.out.println("-- THIS IS On Message method----"+session.hashCode());
+        session.getBasicRemote().sendText("Hi I am Server and you send me a message -------"+message);
+    }
+    
+    
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("-- THIS IS On close  method----"+session.hashCode());
+       
+    }
+    
+}
